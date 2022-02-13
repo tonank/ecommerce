aac3d50565bc73f5a9a6058ba319e07149fa4069
Anton Georgiev <anton.georgiev@microfocus.com>
2022-02-13 21:18:06 +0100

    add docker support and docker-compose orchestration

--- ECommerce.Api.Search/obj/Debug/net6.0/ECommerce.Api.Search.AssemblyInfo.cs
+++ ECommerce.Api.Search/obj/Debug/net6.0/ECommerce.Api.Search.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("30c92094-0497-4f86-9f83-c7bcfffa7bd3")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.Api.Search")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
